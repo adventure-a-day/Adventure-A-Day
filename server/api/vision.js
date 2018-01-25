02a475ebf638e94a75aa89facdf4093a4c97f34b
jasminabasurita <jasminejacquelin@gmail.com>
2018-01-25 18:15:45 -0500

    added post route to /api/vision for imageUrl

--- server/api/vision.js
+++ server/api/vision.js
@@ -1,14 +1,17 @@
 const vision = require("@google-cloud/vision")
-require('../secrets')
-
+const router = require("express").Router()
 const client = new vision.ImageAnnotatorClient()
 
-client
-  .labelDetection("gs://where-in-the-world-bucket/20180125_120946_001.jpg")
-  .then(results => {
-    const labels = results[0].labelAnnotations
+router.post("/", (req, res, next) => {
+  const { imageUrl } = req.body
+  client
+    .labelDetection(imageUrl)
+    .then(results => {
+      const labels = results[0].labelAnnotations
 
-    console.log("Labels: ")
-    labels.forEach(label => console.log(label.description))
-  })
-  .catch(err => console.error("ERROR: ", err))
+      console.log("Labels: ")
+      labels.forEach(label => console.log(label.description))
+      res.send(labels)
+    })
+    .catch(next)
+})
