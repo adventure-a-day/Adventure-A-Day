52c84253c08d6f278970abbc58622ba67d7f99b5
jasminabasurita <jasminejacquelin@gmail.com>
2018-01-30 11:54:24 -0500

    working on creating a verify route

--- server/api/clues.js
+++ server/api/clues.js
@@ -1,6 +1,8 @@
 const router = require("express").Router()
 const { Clue, UserTeamClueStatus } = require("../db/models")
 const isMemberOfTeam = require("../isMemberOfTeam")
+const vision = require("@google-cloud/vision")
+const client = new vision.ImageAnnotatorClient()
 
 module.exports = router
 
@@ -21,4 +23,20 @@ router.get("/:teamId/completedClues", isMemberOfTeam, (req, res, next) => {
 router.get("/:teamId/assignedClues", isMemberOfTeam, (req, res, next) => {
   const assigned = req.clues.filter(clue => clue.status === "assigned")
   res.json(assigned)
+})
+
+router.post("/:teamId/verifyClue", (req, res, next) => {
+  const clue = req.clues.filter(clue => clue.userId === req.user.id)
+  const { imageUrl, geolocation } = req.body
+
+  client
+    .labelDetection(imageUrl)
+    .then(results => {
+      const labels = results[0].labelAnnotations
+
+      console.log("Labels: ")
+      labels.forEach(label => console.log(label.description))
+      res.send(labels)
+    })
+    .catch(next)
 })
