a8d07f375ff649323bd7bda44f55161a1e451621
Annabel Lau <annabel.n.lau@gmail.com>
2018-01-30 13:08:36 -0500

    implemented google web image recognition

--- server/api/clues.js
+++ server/api/clues.js
@@ -29,14 +29,54 @@ router.post("/:teamId/verifyClue", (req, res, next) => {
   //const clue = req.clues.filter(clue => clue.userId === req.user.id)
   const { imageUrl, geolocation } = req.body
 
-  client
-    .labelDetection(imageUrl)
-    .then(results => {
-      const labels = results[0].labelAnnotations
+//   client
+//     .webDetection(imageUrl)
+//     .then(results => {
+//       const labels = results[0].labelAnnotations
 
-      console.log("Labels: ")
-      labels.forEach(label => console.log(label.description))
-      res.send(labels)
-    })
-    .catch(next)
+//       console.log("Labels: ")
+//       labels.forEach(label => console.log(label.description))
+//       res.send(labels)
+//     })
+//     .catch(next)
+// })
+
+client
+.webDetection(imageUrl)
+.then(results => {
+  const webDetection = results[0].webDetection;
+  console.log(webDetection, "webDetection!!!")
+
+  // if (webDetection.fullMatchingImages.length) {
+  //   console.log(
+  //     `Full matches found: ${webDetection.fullMatchingImages.length}`
+  //   );
+  //   webDetection.fullMatchingImages.forEach(image => {
+  //     console.log(`  URL: ${image.url}`);
+  //     console.log(`  Score: ${image.score}`);
+  //   });
+  // }
+
+  // if (webDetection.partialMatchingImages.length) {
+  //   console.log(
+  //     `Partial matches found: ${webDetection.partialMatchingImages.length}`
+  //   );
+  //   webDetection.partialMatchingImages.forEach(image => {
+  //     console.log(`  URL: ${image.url}`);
+  //     console.log(`  Score: ${image.score}`);
+  //   });
+  // }
+
+  if (webDetection.webEntities.length) {
+    console.log(`Web entities found: ${webDetection.webEntities.length}`);
+    webDetection.webEntities.forEach(webEntity => {
+      console.log(`  Description: ${webEntity.description}`);
+      console.log(`  Score: ${webEntity.score}`);
+    });
+  }
+})
+.catch(err => {
+  console.error('ERROR:', err);
+});
+// [END vision_web_detection_gcs]
 })
