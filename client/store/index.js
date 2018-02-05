32a701dc9d49bb7b4121bb878c2f87c3c30d589e
jasminabasurita <jasminejacquelin@gmail.com>
2018-02-05 16:29:42 -0500

    made changes to the deploy script and removed logger for redux if in production

--- client/store/index.js
+++ client/store/index.js
@@ -1,5 +1,6 @@
+const isProduction = process.env.NODE_ENV === 'production'
 import { createStore, combineReducers, applyMiddleware } from "redux"
-// import createLogger from "redux-logger"
+import createLogger from "redux-logger"
 import thunkMiddleware from "redux-thunk"
 import { composeWithDevTools } from "redux-devtools-extension"
 import user from "./user"
@@ -23,9 +24,13 @@ const reducer = combineReducers({
   addMemberMessage,
   currentLocation
 })
-const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware/*, createLogger({ collapsed: true })*/)
-)
+const middleware = isProduction
+  ? composeWithDevTools(
+    applyMiddleware(thunkMiddleware)
+  )
+  : composeWithDevTools(
+    applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
+  )
 const store = createStore(reducer, middleware)
 
 export default store
