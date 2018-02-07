edb2cb3934572d282cdf71afa10bed8dc63586fc
Katharine Chun <katharine.chun@nyu.edu>
2018-02-07 12:33:18 -0500

    added conditional rendering to team home

--- client/components/team-home.js
+++ client/components/team-home.js
@@ -8,51 +8,47 @@ import NewAdventures from "./NewAdventures"
 
 const TeamHome = props => {
   const { clues, teamMembers, user, currentTeam } = props
-  let userClue
+
   return (
     <div>
       <AddTeamMember />
-      <NewAdventures />
-
-      { user && clues.assignedClues && (
-
-        ((clues.assignedClues.filter(clue => clue.userId === user.id)).length > 0) && (
-          <Link to={`/team/${currentTeam.id}/solve-clue`}><button>Complete Current Adventure</button></Link>
-        )
-
-
-        )
-
-
-      }
-
       <h1>Team Adventures</h1>
       <div className="card-container">
-        {clues.assignedClues &&
-          clues.assignedClues.map(clue => {
-            let user
-            if (teamMembers.length) {
-              user = teamMembers.filter(member => member.id === clue.userId)[0]
-            }
-            return (
-              <div key={clue.clue.id} className="card" id={clue.clue.id}>
-                <div className="side">{user && user.userName}</div>
-                <div className="side back">{clue.clue.prompt}</div>
-              </div>
-            )
-          })}
+
+        { clues.assignedClues.length > 0 ?
+            clues.assignedClues.map(clue => {
+              let member
+              if (teamMembers.length) {
+                member = teamMembers.filter(member => member.id === clue.userId)[0]
+              }
+              return (
+                <div key={clue.clue.id} className="card" id={clue.clue.id}>
+                  <div className="side">{member && member.userName}</div>
+                  <div className="side back">
+                    <div>{clue.clue.prompt}
+                    <br/>
+                    {user.id === clue.userId && clues.assignedClues && (
+                      <Link to={`/team/${currentTeam.id}/solve-clue`}><button>Upload Photo</button></Link>
+                    )}
+                    </div>
+                  </div>
+                </div>
+              )
+            })
+          : <NewAdventures />}
+      
       </div>
       <h1>Completed Adventures</h1>
       <div className="card-container">
         {clues.completedClues &&
           clues.completedClues.map(clue => {
-            let user
+            let member
             if (teamMembers.length) {
-              user = teamMembers.filter(member => member.id === clue.userId)[0]
+              member = teamMembers.filter(member => member.id === clue.userId)[0]
             }
             return (
               <div key={clue.clue.id} className="card" id={clue.clue.id}>
-                <div className="side">{user && user.userName}</div>
+                <div className="side">{member && member.userName}</div>
                 <div className="side back">{clue.clue.prompt}</div>
               </div>
             )
