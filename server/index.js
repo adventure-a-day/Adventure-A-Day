802636cddad8e085609fdb1459c79fecd5d9a93a
Jasmina Jacquelina <jasminejacquelin@gmail.com>
2018-02-28 16:09:45 -0500

    disabling https redirect

--- server/index.js
+++ server/index.js
@@ -35,15 +35,15 @@ passport.deserializeUser((id, done) =>
 )
 
 const createApp = () => {
-  if (process.env.NODE_ENV === "production") {
-    app.use((req, res, next) => {
-      if (req.header("x-forwarded-proto") !== "https") {
-        res.redirect(`https://${req.header("host")}${req.url}`)
-      } else {
-        next()
-      }
-    })
-  }
+  // if (process.env.NODE_ENV === "production") {
+  //   app.use((req, res, next) => {
+  //     if (req.header("x-forwarded-proto") !== "https") {
+  //       res.redirect(`https://${req.header("host")}${req.url}`)
+  //     } else {
+  //       next()
+  //     }
+  //   })
+  // }
 
   // logging middleware
   app.use(morgan("dev"))
